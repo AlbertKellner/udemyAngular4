727f7d6d3dc3cabd646dbbe80b9af5a9dcd7390f
Albert Kellner Lescinskiene <albert.kellner@gmail.com>
2017-08-29 18:37:34 -0300

    Aula 50 - *ngFor

--- src/app/security.ts
+++ src/app/security.ts
@@ -1,4 +1,4 @@
 export interface Security {
     trade();
-    fairValueCompute();
+    fairValueCompute(); 
 }
--- src/app/stocks.component.ts
+++ src/app/stocks.component.ts
@@ -10,6 +10,14 @@ import { StockService } from "./stock.service";
                 {{stock}}
             </li>
         </ul>
+
+        <hr>
+
+        <ul>
+            <li *ngFor="let stocksMarket of stocksMarkets">
+                {{stocksMarket}}
+            </li>
+        </ul>
     `
 })
 
@@ -17,6 +25,8 @@ export class StocksComponent {
     title = 'list of Stocks';
     //stocks = ['APPL', 'IMB', 'GOOG', 'UDEMY'];
     stocks;
+
+    stocksMarkets = ['NYSE', 'NASDAQ', 'EURONEXT', 'HKSE', 'LSE']
 
     constructor(stockService: StockService) {
         this.stocks = stockService.getStocks();
